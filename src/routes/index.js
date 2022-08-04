747224ae622e134c40b206a0f8cc8e16889531ac
felpsaints <felipesaints94@gmail.com>
2022-08-03 22:47:28 -0300

    configuração inicial do express 5 pagina falecosnosco

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/perfilDoUsuarioMeuEndereco',(req, res) => {
-  res.render('perfilDoUsuarioMeuEndereco');
+router.get('/inscricao',(req, res) => {
+  res.render('inscricao');
 });
+
+router.get('/perfilDoUsuarioHistoricoDePedidos',(req, res) => {
+  res.render('perfilDoUsuarioHistoricoDePedidos');
+});
+
+router.get('/faleConosco',(req, res) => {
+  res.render('faleConosco');
+});
+
 
 module.exports = router;
