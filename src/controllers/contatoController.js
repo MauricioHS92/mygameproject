c4390659cabda84f92f5241ee6d8bb8880a7ecb6
felpsaints <felipesaints94@gmail.com>
2022-08-20 01:23:19 -0300

    continuando crud fale conosco

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const {v4} = require('uuid');
+const db = require('../database/db.json');
+
 const contatoController = {
     formularioFaleConosco: (req, res) => {
         res.render('faleConosco');
@@ -5,6 +9,12 @@ const contatoController = {
 
     enviar: (req, res) => {
         const formFc = req.body;
+        db.msgcontato.push({
+            id: v4(),
+            ...formFc,
+        });
+        const json = JSON.stringify(db);
+        fs.writeFileSync('src/database/db.json', json);
     }
 }
 
