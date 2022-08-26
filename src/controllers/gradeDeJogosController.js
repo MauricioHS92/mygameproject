4299494ebeb92502b1f0277f561688b65b483695
jp-data <joaopaulo94.economia@gmail.com>
2022-08-25 23:51:09 -0300

    grade de jogos - controller

--- src/controllers/gradeDeJogosController.js
+++ src/controllers/gradeDeJogosController.js
@@ -2,6 +2,10 @@ const gradeDeJogosController =  {
     viewPage: (req, res) =>{
         res.render('telaGradeDeJogos');
     },
+    trazerJogos: (req, res) =>{
+        const json = JSON.stringify(db);
+        fs.writeFileSync('src/database/db.json', json);
+    }
 }
 
 module.exports = gradeDeJogosController;
