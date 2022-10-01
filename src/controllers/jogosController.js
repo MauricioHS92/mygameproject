8dafb77abac85cd0fef0279a0e13382308cb0f48
Jhon <joaopaulo94.economia@gmail.com>
2022-09-30 22:54:16 -0300

    sequelize tela de jogo

--- src/controllers/jogosController.js
+++ src/controllers/jogosController.js
@@ -1,42 +1,47 @@
-const {Jogo} = require('../../models');
+const { Jogo } = require('../../models');
 
-const jogosController =  {
+const jogosController = {
 
-     trazerJogos: async (req, res) => {
-       try {
-        const jogos = await Jogo.findAll();
-        res.render('telaGradeDeJogos', {jogos});
-       } catch(error) {
-        console.log(error)
-       }
-        
+    trazerJogos: async (req, res) => {
+        try {
+            const jogos = await Jogo.findAll();
+            res.render('telaGradeDeJogos', { jogos });
+        } catch (error) {
+            console.log(error)
+        }
+
     },
 
-    selecionarJogo: (req, res) => {
-        const {id} = req.params;
-        const jogoSelecionado = Jogo.findById(id);
-        res.render('telaDeJogo', {jogoSelecionado});
-    }}
-
-    listarJogos: (req, res) =>{
-        // const {nomeDoJogo} = req.params;
-        // if({nomeDoJogo} == "esportes"){
-        //     let listaDeJogos = Jogo.esportes()
-        //     // res.render('telaGradeDeJogos', {listaDeJogos})
-        // }
-        // console.log('Olá' + listaDeJogos)
-        // if({name} == "esportes"){
-        //     let listaDeJogos = jogo.esportes()
-        //     res.render('telaGradeDeJogos', {listaDeJogos})
-        // }
-        // if({name} == "fps"){
-        //     let listaDeJogos = jogo.fps()
-        //     res.render('telaGradeDeJogos', {listaDeJogos})
-        // }
-        // if({name} == "outros"){
-        //     let listaDeJogos = jogo.outros()
-        //     res.render('telaGradeDeJogos', {listaDeJogos})
+    selecionarJogo: async (req, res) => {
+        try {
+            const { id } = await req.params;
+            const jogoSelecionado = await Jogo.findById(id);
+            res.render('telaDeJogo', { jogoSelecionado });
+        } catch (error) {
+            console.log(error)
         }
+    }
+}
+
+listarJogos: (req, res) => {
+    // const {nomeDoJogo} = req.params;
+    // if({nomeDoJogo} == "esportes"){
+    //     let listaDeJogos = Jogo.esportes()
+    //     // res.render('telaGradeDeJogos', {listaDeJogos})
+    // }
+    // console.log('Olá' + listaDeJogos)
+    // if({name} == "esportes"){
+    //     let listaDeJogos = jogo.esportes()
+    //     res.render('telaGradeDeJogos', {listaDeJogos})
+    // }
+    // if({name} == "fps"){
+    //     let listaDeJogos = jogo.fps()
+    //     res.render('telaGradeDeJogos', {listaDeJogos})
+    // }
+    // if({name} == "outros"){
+    //     let listaDeJogos = jogo.outros()
+    //     res.render('telaGradeDeJogos', {listaDeJogos})
+}
 
 
 module.exports = jogosController;
