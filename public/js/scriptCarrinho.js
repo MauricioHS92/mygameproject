9ce09cc99f9d21a6ec7c00bbcd8c8039a197fd04
MauricioHS92 <hansenmailper@gmail.com>
2022-10-21 21:35:24 -0300

    continuacao DOM

--- public/js/scriptCarrinho.js
+++ public/js/scriptCarrinho.js
@@ -1,6 +1,7 @@
 const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
 function addItemCart(product) {
+    console.log('aquiAddCart');
     carrinho.push(product);
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
 }
@@ -12,3 +13,29 @@ function removeItemCart(product) {
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
 }
 
+function renderProducts(products) {
+    const mainEl = document.querySelector("main");
+    products.forEach((product) => {
+      const productEl = document.createElement("div");
+      productEl.classList.add("card");
+      const productString = `{
+        "id": ${product.id},
+        "title": ${product.title},
+        "price": ${product.price},
+        "image": ${product.image}
+      }`;
+      productEl.innerHTML = `
+          <a class="fav" onclick='toggleFavorite(${productString})'>
+            <img src="icons/heart_red_empty.svg">
+          </a>
+          <img class="product" src="${product.image}">
+          <h1>${product.title}</h1>
+          <div class="price">R$ ${product.price}</div>
+          <div class="buttons">
+            <button>Comprar</button>
+            <button>Ver Mais</button>
+          </div>
+        `;
+      mainEl.appendChild(productEl);
+    });
+  }
--- src/controllers/jogosController.js
+++ src/controllers/jogosController.js
@@ -1,27 +1,28 @@
-const { Jogo } = require('../../models');
+const { Jogo } = require("../../models");
 
 const jogosController = {
-
-    trazerJogos: async (req, res) => {
-        try {
-            const jogos = await Jogo.findAll();
-            res.render('telaGradeDeJogos', { jogos });
-            console.log(jogos)
-        } catch (error) {
-            console.log(error)
-        }
-
-    },
-
-    //Criando método de selecionar jogo
-    escolherJogo: async (req, res) => {
-        try{
-            const {id} = req.params;
-            const jogo = await Jogo.findByPk(id);
-            res.render('telaDeJogo', {jogo});
-        } catch ( error ) {
-            console.log( error )
-        }
+  trazerJogos: async (req, res) => {
+    try {
+      const jogos = await Jogo.findAll();
+      const jogosComJson = jogos.map(jogo => {
+        jogo.id_jogo, jogo.nome, jogo.imagem, jogo.preco;
+      });
+      res.render("telaGradeDeJogos", { jogos });
+      console.log(jogos);
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+
+  //Criando método de selecionar jogo
+  escolherJogo: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const jogo = await Jogo.findByPk(id);
+      res.render("telaDeJogo", { jogo });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 module.exports = jogosController;
