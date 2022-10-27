4a64f2a8150f4917b90779d0dd22dbb2faa1d782
BrunoBarajs <barajs@gmail.com>
2022-10-26 22:29:20 -0300

    Nova tentativa de implementação de DOM na Fale Conosco

--- public/js/scriptFaleConosco.js
+++ public/js/scriptFaleConosco.js
@@ -17,4 +17,22 @@ function checkValues() {
 function fieldError() {
     let fieldEmail = document.getElementById("email")
     fieldEmail.style.borderColor = "red"
-}
+};
+
+
+
+/*
+input.onblur = function() {
+    if (!input.value.includes('@')) {
+    input.classList.add('invalid');
+    error.innerHTML = "Insira um e-mail válido."
+    }
+};
+
+input.onfocus = function() {
+    if (this.classList.contains("invalid")) {
+    this.classList.remove("invalid");
+    error.innerHTML = "";
+    }
+};
+*/
