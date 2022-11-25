6e0e25ecdc94548cb5e2ea4d4088edbf7dc4ee81
MauricioHS92 <hansenmailper@gmail.com>
2022-11-24 22:59:05 -0300

    alteracoes css login, perfil usuario e theme

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 const config = {
     username: 'root',
-    password: '3240',
+    password: '',
     database: 'pi', //o nome do banco que foi criado no mysql
     host: 'localhost',
     dialect: 'mysql' 
